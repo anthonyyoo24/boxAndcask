107a698de0fb0b2a4034e562262943907d17d09e
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2020-09-10 21:44:47 -0400

    Add validation for product create redux form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import ProductDetails from './product/ProductDetails';
+import ProductCreate from './product/ProductCreate';
 import ProductList from './product/ProductList';
+import ProductEdit from './product/ProductEdit';
+import ProductDelete from './product/ProductDelete';
 import Header from './Header';
 
 const App = () => {
@@ -10,8 +13,13 @@ const App = () => {
       <HashRouter>
         <div>
           <Header />
-          <Route path="/" exact component={ProductList} />
-          <Route path="/product/:id" exact component={ProductDetails} />
+          <Switch>
+            <Route path="/" exact component={ProductList} />
+            <Route path="/product/new" exact component={ProductCreate} />
+            <Route path="/product/edit/:id" exact component={ProductEdit} />
+            <Route path="/product/delete/:id" exact component={ProductDelete} />
+            <Route path="/product/:id" exact component={ProductDetails} />
+          </Switch>
         </div>
       </HashRouter>
     </div>
