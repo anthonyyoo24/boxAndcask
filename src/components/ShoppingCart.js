a3e97badc128761c22ab0bd6e83a1dd8aea74462
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2020-09-24 20:16:16 -0400

    Take css and img folders out of components folder

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,4 @@
-import './css/style.css';
+import '../css/style.css';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeQuantity, emptyCart, paymentFail, removeFromCart } from '../actions';
--- src/components/product/ProductDetails.js
+++ src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import '../css/style.css';
+import '../../css/style.css';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
