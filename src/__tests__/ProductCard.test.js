b08f1f05cae5aba7b4afe4ce98781dbed38ba2ef
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2021-01-14 18:31:02 -0500

    Wrap tests in another nested describe corresponding to the case where props are passed in and add another for when there are no props

--- src/__tests__/ProductCard.test.js
+++ src/__tests__/ProductCard.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import ProductCard from '../components/product/ProductCard';
@@ -11,47 +10,63 @@ const fakeProduct = {
 };
 
 describe('ProductCard', () => {
-  let productCardWrapper;
+  describe('With props', () => {
+    let productCardWrapper;
+    beforeAll(() => {
+      productCardWrapper = shallow(<ProductCard product={fakeProduct} />);
+    });
 
-  beforeAll(() => {
-    productCardWrapper = shallow(<ProductCard product={fakeProduct} />);
+    it('renders without error', () => {
+      expect(productCardWrapper.find('.product-card')).toHaveLength(1);
+    });
+
+    it('renders the price', () => {
+      const price = productCardWrapper.find('.product-card__price');
+
+      expect(price).toHaveLength(1);
+      expect(price.text()).toBe(`$${fakeProduct.price}`);
+    });
+
+    it('renders the image', () => {
+      const img = productCardWrapper.find('img');
+
+      expect(img).toHaveLength(1);
+      expect(img.props().src).toBe(fakeProduct.image);
+      expect(img.props().alt).toBe(fakeProduct.name);
+      expect(img.props().className).toBe('product-card__img');
+    });
+
+    it('renders the links', () => {
+      const links = productCardWrapper.find('Link');
+
+      expect(links).toHaveLength(2);
+      expect(links.find('img').exists()).toBe(true);
+      expect(links.at(0).props().to).toBe(`/products/${fakeProduct.id}`);
+      expect(links.at(1).props().to).toBe(`/products/${fakeProduct.id}`);
+      expect(links.at(1).props().className).toBe('product-card__name');
+      expect(links.at(1).text()).toBe(fakeProduct.name);
+    });
+
+    it('matches the snapshot', () => {
+      expect(toJSON(productCardWrapper)).toMatchSnapshot();
+    });
+
+    it('updates via props', () => {
+      const productCardWrapper = shallow(<ProductCard product={{ ...fakeProduct, price: 1000 }} />);
+      expect(toJSON(productCardWrapper)).toMatchSnapshot();
+      productCardWrapper.setProps({ product: { ...fakeProduct, price: 1100.99 } });
+      expect(toJSON(productCardWrapper)).toMatchSnapshot();
+    });
   });
 
-  it('renders the price', () => {
-    const price = productCardWrapper.find('.product-card__price');
+  describe('Without props', () => {
+    let productCardWrapper;
+    beforeAll(() => {
+      productCardWrapper = shallow(<ProductCard />);
+    });
 
-    expect(price).toHaveLength(1);
-    expect(price.text()).toBe(`$${fakeProduct.price}`);
-  });
-
-  it('renders the image', () => {
-    const img = productCardWrapper.find('img');
-
-    expect(img).toHaveLength(1);
-    expect(img.props().src).toBe(fakeProduct.image);
-    expect(img.props().alt).toBe(fakeProduct.name);
-    expect(img.props().className).toBe('product-card__img');
-  });
-
-  it('renders the links', () => {
-    const links = productCardWrapper.find('Link');
-
-    expect(links).toHaveLength(2);
-    expect(links.find('img').exists()).toBe(true);
-    expect(links.at(0).props().to).toBe(`/products/${fakeProduct.id}`);
-    expect(links.at(1).props().to).toBe(`/products/${fakeProduct.id}`);
-    expect(links.at(1).props().className).toBe('product-card__name');
-    expect(links.at(1).text()).toBe(fakeProduct.name);
-  });
-
-  it('matches the snapshot', () => {
-    expect(toJSON(productCardWrapper)).toMatchSnapshot();
-  });
-
-  it('updates via props', () => {
-    const productCardWrapper = shallow(<ProductCard product={{ ...fakeProduct, price: 1000 }} />);
-    expect(toJSON(productCardWrapper)).toMatchSnapshot();
-    productCardWrapper.setProps({ product: { ...fakeProduct, price: 1100.99 } });
-    expect(toJSON(productCardWrapper)).toMatchSnapshot();
+    it('should not render', () => {
+      expect(productCardWrapper.find('.product-card')).toHaveLength(0);
+    });
   });
 });
