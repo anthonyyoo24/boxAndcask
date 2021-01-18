96f4930dc490bb86b8c69b623ecfa48ccdf635cf
anthonyyoo91 <anthony_yoo_24@hotmail.com>
2021-01-17 20:02:11 -0500

    Remove all redundant tests

--- src/reducers/productReducer.test.js
+++ src/reducers/productReducer.test.js
@@ -27,135 +27,54 @@ describe('Product Reducer', () => {
     });
   });
 
-    it('returns the new state that includes the newly edited product when receiving type EDIT_PRODUCT', () => {
-      const prevState = { 1: { id: 1, name: 'Josephine Sofa', price: 1200.99, stock: 7 } };
+  it('returns the new state that includes the newly edited product when receiving type EDIT_PRODUCT', () => {
+    const prevState = { 1: { id: 1, name: 'Josephine Sofa', price: 1200.99, stock: 7 } };
 
-      expect(newState(productReducer, prevState, EDIT_PRODUCT, product)).toEqual({
-        [product.id]: product,
-      });
-    });
-
-  describe('When the action object has type DELETE_PRODUCT', () => {
-    const testInvalidCases = (prevState = {}) => {
-      it('returns just the previous state when there is no action payload', () => {
-        expect(newState(productReducer, prevState, DELETE_PRODUCT)).toEqual(prevState);
-      });
-      it('returns just the previous state when the product to delete is not in the previous state', () => {
-        expect(newState(productReducer, prevState, DELETE_PRODUCT, 10)).toEqual(prevState);
-      });
-    };
-
-    describe('When the previous state is empty', () => {
-      testInvalidCases();
-    });
-
-    describe('When the previous state has one product', () => {
-      const prevState = { 1: { id: 1, name: 'Cavette Sofa', price: 1200.99, stock: 7 } };
-
-      testInvalidCases(prevState);
-
-      it('returns an empty state when you delete the product in the previous state', () => {
-        expect(newState(productReducer, prevState, DELETE_PRODUCT, 1)).toEqual({});
-      });
-    });
-
-    describe('When the previous state has two products', () => {
-      const prevState = {
-        1: { id: 1, name: 'Cavette Sofa', price: 1200.99, stock: 7 },
-        2: { id: 2, name: 'Josephine Sofa', price: 1500.99, stock: 20 },
-      };
-
-      testInvalidCases(prevState);
-
-      it('returns a new state that includes the one other product that was not deleted', () => {
-        expect(newState(productReducer, prevState, DELETE_PRODUCT, 1)).toEqual({
-          2: { id: 2, name: 'Josephine Sofa', price: 1500.99, stock: 20 },
-        });
-      });
+    expect(newState(productReducer, prevState, EDIT_PRODUCT, product)).toEqual({
+      [product.id]: product,
     });
   });
 
+  it('returns the new state that does not contain the deleted product when receiving type DELETE_PRODUCT', () => {
+    const prevState = { 1: { id: 1, name: 'Cavette Sofa', price: 1200.99, stock: 7 } };
+
+    expect(newState(productReducer, prevState, DELETE_PRODUCT, 1)).toEqual({});
+  });
+
   describe('When the action object has type FETCH_PRODUCT', () => {
-    const fetchProductTest = (prevState = {}) => {
-      it('returns just the previous state when the fetched product does not exist', () => {
-        expect(newState(productReducer, prevState, FETCH_PRODUCT)).toEqual(prevState);
-      });
-
-      it('returns the new state that has the fetched product', () => {
-        const product = { id: 1, name: 'Magritte Sofa', price: 1599.99, stock: 10 };
-
-        expect(newState(productReducer, prevState, FETCH_PRODUCT, product)).toEqual({
-          ...prevState,
-          1: product,
-        });
-      });
-    };
-
-    describe('When the previous state is empty', () => {
-      fetchProductTest();
+    it('returns just the previous state when the fetched product does not exist', () => {
+      expect(newState(productReducer, {}, FETCH_PRODUCT)).toEqual({});
     });
 
-    describe('When the previous state has one product', () => {
-      const prevState = { 2: { id: 2, name: 'Cavette Sofa', price: 1200.99, stock: 7 } };
-
-      fetchProductTest(prevState);
-    });
-
-    describe('When the previous state has two products', () => {
-      const prevState = {
-        2: { id: 2, name: 'Cavette Sofa', price: 1200.99, stock: 7 },
-        3: { id: 3, name: 'Josephine Sofa', price: 500.99, stock: 9 },
-      };
-
-      fetchProductTest(prevState);
+    it('returns the new state that has the fetched product', () => {
+      expect(newState(productReducer, {}, FETCH_PRODUCT, product)).toEqual({
+        [product.id]: product,
+      });
     });
   });
 
   describe('When the action object has type FETCH_PRODUCTS', () => {
-    const fetchProductsTest = (prevState = {}) => {
-      it('returns just the previous state when the product list to fetch is empty', () => {
-        expect(newState(productReducer, prevState, FETCH_PRODUCTS, [])).toEqual(prevState);
-      });
-
-      it('returns the new state when there is one fetched product', () => {
-        const products = [{ id: 1, name: 'Magritte Sofa', price: 1599.99, stock: 10 }];
-
-        expect(newState(productReducer, prevState, FETCH_PRODUCTS, products)).toEqual({
-          ...prevState,
-          1: products[0],
-        });
-      });
-
-      it('returns the new state when there are two fetched products', () => {
-        const products = [
-          { id: 1, name: 'Magritte Sofa', price: 1599.99, stock: 10 },
-          { id: 2, name: 'Josephine Sofa', price: 999.99, stock: 12 },
-        ];
-
-        expect(newState(productReducer, prevState, FETCH_PRODUCTS, products)).toEqual({
-          ...prevState,
-          ..._.mapKeys(products, 'id'),
-        });
-      });
-    };
-
-    describe('When the previous state is empty', () => {
-      fetchProductsTest();
+    it('returns just the previous state when the product list to fetch is empty', () => {
+      expect(newState(productReducer, {}, FETCH_PRODUCTS, [])).toEqual({});
     });
 
-    describe('When the previous state has one product', () => {
-      const prevState = { 1: { id: 1, name: 'Cavette Sofa', price: 650.99, stock: 5 } };
+    it('returns the new state when there is one fetched product', () => {
+      const products = [{ id: 1, name: 'Magritte Sofa', price: 1599.99, stock: 10 }];
 
-      fetchProductsTest(prevState);
+      expect(newState(productReducer, {}, FETCH_PRODUCTS, products)).toEqual({
+        1: products[0],
+      });
     });
 
-    describe('When the previous state has two products', () => {
-      const prevState = {
-        3: { id: 3, name: 'Cavette Sofa', price: 650.99, stock: 5 },
-        4: { id: 4, name: 'Mimi Sofa', price: 800.99, stock: 12 },
-      };
+    it('returns the new state when there are two fetched products', () => {
+      const products = [
+        { id: 1, name: 'Magritte Sofa', price: 1599.99, stock: 10 },
+        { id: 2, name: 'Josephine Sofa', price: 999.99, stock: 12 },
+      ];
 
-      fetchProductsTest(prevState);
+      expect(newState(productReducer, {}, FETCH_PRODUCTS, products)).toEqual({
+        ..._.mapKeys(products, 'id'),
+      });
     });
   });
 });
